6af6a613bb11fd1f5125cccc3ad612ac0c5c9572
vanloiidk <vanloiidk@gmail.com>
2019-10-20 16:20:45 +0700

    streaming page

--- src/scripts/streaming.js
+++ src/scripts/streaming.js
@@ -1,5 +1,7 @@
 const getStreamingKey = localStorage.getItem('key');
-const source = `http://127.0.0.1:3002/live/loi/index.m3u8`;
+const source = `http://192.168.10.109:3002/live/loi/index.m3u8`;
+const servers = Array.from(document.querySelectorAll('.server-bt'))
+
 
 const video = document.querySelector('.streamVideo');
 // const liveBT = document.querySelector('.live')
@@ -8,17 +10,49 @@ const video = document.querySelector('.streamVideo');
 //     video.currentTime = duration-5
 // })
 
-if (Hls.isSupported()) {
-    var hls = new Hls();
-    hls.loadSource(source);
-    hls.attachMedia(video);
-    hls.on(Hls.Events.MANIFEST_PARSED, function () {
-        video.play();
-    });
+servers[0].dataset.source =`http://192.168.10.109:3002/live/loi/index.m3u8`
+servers[1].dataset.source =`http://192.168.10.109:3002/live/khang/index.m3u8`
+
+
+for (var i=0, max=servers.length; i < max; i++) {
+    // Do something with the element here
+    servers[i].addEventListener("click",function(){
+        const a = this.dataset.source
+        removeClass("active")
+        this.classList.add("active")
+        loadHLS(a)
+    })
+
+};
+
+
+
+loadHLS(source)
+
+function getStreamingURL(){
+    
 }
-else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-    video.src = 'https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8';
-    video.addEventListener('loadedmetadata', function () {
-        video.play();
-    });
+
+function removeClass(classname){
+    console.log(servers)
+    for (var i=0, max=servers.length; i < max; i++) {
+        servers[i].classList.remove(classname)
+    }
+}
+
+function loadHLS(source){
+    if (Hls.isSupported()) {
+        var hls = new Hls();
+        hls.loadSource(source);
+        hls.attachMedia(video);
+        hls.on(Hls.Events.MANIFEST_PARSED, function () {
+            video.play();
+        });
+    }
+    else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+        video.src = 'https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8';
+        video.addEventListener('loadedmetadata', function () {
+            video.play();
+        });
+    }
 }
