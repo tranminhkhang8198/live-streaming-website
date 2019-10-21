4365c3ef9a4665baac654503e41af44cc1f86412
vanloiidk <vanloiidk@gmail.com>
2019-10-22 04:05:12 +0700

    videojs-hls

--- src/scripts/streaming.js
+++ src/scripts/streaming.js
@@ -1,17 +1,45 @@
 const getStreamingKey = localStorage.getItem('key');
-const source = `http://f82f3403.ngrok.io/live/loi/index.m3u8`;
+const source = `http://localhost:3002/live/loi/index.m3u8`;
 const servers = Array.from(document.querySelectorAll('.server-bt'))
 
+////-- init player
+var options = {
+    html5: {
+        hlsjsConfig: {
+          // Put your hls.js config here
+        }
+    }
+};
 
-const video = document.querySelector('.streamVideo');
+// setup beforeinitialize hook
+videojs.Html5Hlsjs.addHook('beforeinitialize', (videojsPlayer, hlsjsInstance) => {
+    // here you can interact with hls.js instance and/or video.js playback is initialized
+});
+
+var player = videojs('video-hls', options);
+
+////
+
+
+player.on('pause', function() {
+    this.bigPlayButton.show();
+  
+    // Now the issue is that we need to hide it again if we start playing
+    // So every time we do this, we can create a one-time listener for play events.
+    video.one('play', function() {
+      this.bigPlayButton.hide();
+    });
+  });
+
+// const video = videojs('stream-video');
 // const liveBT = document.querySelector('.live')
 // liveBT.addEventListener("click",function(){
 //     duration = video.duration
 //     video.currentTime = duration-5
 // })
 
-servers[0].dataset.source =`http://f82f3403.ngrok.io/live/loi/index.m3u8`
-servers[1].dataset.source =`http://f82f3403.ngrok.io/live/khang/index.m3u8`
+servers[0].dataset.source =`http://localhost:3002/live/loi/index.m3u8`
+servers[1].dataset.source =`http://127.0.0.1:3002/live/khang/index.m3u8`
 
 
 for (var i=0, max=servers.length; i < max; i++) {
@@ -27,11 +55,15 @@ for (var i=0, max=servers.length; i < max; i++) {
 
 
 
-loadHLS(source)
 
 function getStreamingURL(){
     
 }
+function loadHLS(source,player){
+    player.src({type: 'application/x-mpegURL', src: source});
+    player.play()
+}
+
 
 function removeClass(classname){
     console.log(servers)
@@ -39,20 +71,5 @@ function removeClass(classname){
         servers[i].classList.remove(classname)
     }
 }
+loadHLS(source, player)
 
-function loadHLS(source){
-    if (Hls.isSupported()) {
-        var hls = new Hls();
-        hls.loadSource(source);
-        hls.attachMedia(video);
-        hls.on(Hls.Events.MANIFEST_PARSED, function () {
-            video.play();
-        });
-    }
-    else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-        video.src = 'https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8';
-        video.addEventListener('loadedmetadata', function () {
-            video.play();
-        });
-    }
-}
